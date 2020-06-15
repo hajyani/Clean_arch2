568e5a8cd103daf79f8dc63df4ce722ce104ab25
alireza haji <Alireza.hajyani1395@hotmail.com>
2020-06-15 11:19:38 +0430

    Course Repository and Service

---
+++ CleanArch/CleanArch.Core/Services/CourseService.cs
@@ -0,0 +1,27 @@
+﻿using CleanArch.Core.Interfaces;
+using CleanArch.Core.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Core.Services
+{
+    public class CourseService : ICourseService
+    {
+        private ICourseRepository _courseRepository;
+
+        public CourseService( ICourseRepository courseRepository )
+        {
+            _courseRepository = courseRepository;
+        }
+
+        public CourseViewModel GetCourses()
+        {
+            return new CourseViewModel()
+            {
+                Courses = _courseRepository.GetCourses()
+            };
+        }
+    }
+}
