fe9724f971ddaaad6e7f556d2c2b231063a6faea
alireza haji <Alireza.hajyani1395@hotmail.com>
2020-06-15 08:15:01 +0430

    Initial Core Layer

---
+++ CleanArch/CleanArch.Core/Interfaces/ICourseService.cs
@@ -0,0 +1,12 @@
+﻿using CleanArch.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Core.Interfaces
+{
+    public interface ICourseService
+    {
+        CourseViewModel GetCourses();
+    }
+}
